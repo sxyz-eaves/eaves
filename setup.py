59bf40f82ac8ef8ab55a13e86ed1046def6bef51
shiyihang2007 <467557146@qq.com>
2023-02-20 13:58:29 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="eaves",
+    version="0.0.1",
+    authors="sxyz-eaves",
+    author_email="",
+    description="一个基于 python 的轻量级比赛创建辅助工具。",
+    url="https://github.com/sxyz-eaves/eaves",
+    packages=["SampleContest", ""],
+)
