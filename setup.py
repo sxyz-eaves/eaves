d072c1e187618fb248897ceea699c6a6729e593f
shiyihang2007 <467557146@qq.com>
2023-02-20 15:13:56 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ setuptools.setup(
     name = "eaves",
     version = "0.0.1",
     author = "sxyz-eaves",
-    author_email = "467557146@qq.com",
+    author_email = "xxx@xxx.com",
     description = "一个基于 python 的轻量级比赛创建辅助工具。",
     long_description = "一个基于 python 的轻量级比赛创建辅助工具。",
     long_description_content_type="text/markdown",  # 模块详细介绍格式
-    # url="https://github.com/wupeiqi/fucker",  # 模块github地址
+    # url="https://github.com/sxyz-eaves/eaves",  # 模块github地址
     packages=setuptools.find_packages(),  # 自动找到项目中导入的模块
     # 模块相关的元数据
     classifiers=[
