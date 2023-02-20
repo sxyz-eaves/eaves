09f0599e625592984bfa154644dabefa7022a70e
caijiMK <1395354790@qq.com>
2023-02-21 07:44:14 +0800

    优化 unknownParameter 和 missingParameter

--- eaves/base.py
+++ eaves/base.py
@@ -28,13 +28,19 @@ def settingLog():
 
 def unknownParameter(parameter):
 	# 当发现未知参数时可调用此函数，并直接停止运行程序。
-	log.error(u'发现未知参数 %s，程序停止运行。', parameter)
+	log.error(
+		u'发现未知参数 %s，程序停止运行。请使用 \'eaves.%s help\' 查询使用方式。' % (
+			parameter,
+			os.splitext(os.path.split(__main__.__file__)[1])[0]
+			# 把文件名切出来
+		)
+	)
 	exit()
 
 def missingParameter():
 	# 发现缺少必要参数，直接停止运行程序。
 	log.error(
-		u'缺少必要参数，使用 \'eaves.%s -h\' 查询使用方式。' % (
+		u'缺少必要参数，程序停止运行。请使用 \'eaves.%s help\' 查询使用方式。' % (
 			os.splitext(os.path.split(__main__.__file__)[1])[0]
 			# 把文件名切出来
 		)
