5e5b936587ab93071a9ac52674510c087de59258
shiyihang2007 <467557146@qq.com>
2023-02-20 15:10:49 +0800

    更改了文件目录

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
-from distutils.core import setup
-
-setup(
-    name="eaves",
-    version="0.0.1",
-    authors="sxyz-eaves",
-    author_email="",
-    description="一个基于 python 的轻量级比赛创建辅助工具。",
-    url="https://github.com/sxyz-eaves/eaves",
-    packages=["SampleContest", ""],
+import setuptools
+with open("README.md", "r") as fileDescription:
+    longDescription = fileDescription.read(),
+setuptools.setup(
+    name = "eaves",
+    version = "0.0.1",
+    author = "sxyz-eaves",
+    author_email = "467557146@qq.com",
+    description = "一个基于 python 的轻量级比赛创建辅助工具。",
+    long_description = longDescription,
+    long_description_content_type="text/markdown",  # 模块详细介绍格式
+    # url="https://github.com/wupeiqi/fucker",  # 模块github地址
+    packages=setuptools.find_packages(),  # 自动找到项目中导入的模块
+    # 模块相关的元数据
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL-3.0 License",
+        "Operating System :: OS Independent",
+    ],
+    # 依赖模块
+    install_requires=[
+    ],
+    python_requires='>=3',
 )
