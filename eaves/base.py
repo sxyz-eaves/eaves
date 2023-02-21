c5d1a44c87f01d7f4fd72db3319f334f7a6a69d2
caijiMK <1395354790@qq.com>
2023-02-21 14:46:53 +0800

    将版权声明改为log.info

--- eaves/base.py
+++ eaves/base.py
@@ -34,12 +34,12 @@ def init():
 			__main__.__file__, os.getcwd(), str(sys.argv[1:]), str(datetime.datetime.now())
 		)
 	)
-	print(u'版权声明')
-	print(u'  版权所有 (C) 2023 SXYZ-EAVES 团队 保留一切权利 ')
-	print(u'  此程序遵循 GPL-3.0 许可协议 ')
-	print(u'  您可以遵照自由软件基金会出版的 GNU通用公共许可证条款 来修改和重新发布这一程序 ')
-	print(u'  发布这一程序的目的是希望它有用 但它没有任何担保 甚至没有适合特定目的的隐含的担保 ')
-	print(u'  更详细的情况请参阅 GNU通用公共许可证条款 ')
+	log.info(u'版权声明')
+	log.info(u'  版权所有 (C) 2023 SXYZ-EAVES 团队 保留一切权利 ')
+	log.info(u'  此程序遵循 GPL-3.0 许可协议 ')
+	log.info(u'  您可以遵照自由软件基金会出版的 GNU通用公共许可证条款 来修改和重新发布这一程序 ')
+	log.info(u'  发布这一程序的目的是希望它有用 但它没有任何担保 甚至没有适合特定目的的隐含的担保 ')
+	log.info(u'  更详细的情况请参阅 GNU通用公共许可证条款 ')
 
 def unknownParameter(parameter):
 	# 当发现未知参数时可调用此函数，并直接停止运行程序。
