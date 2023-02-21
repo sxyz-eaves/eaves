c62cf55a1840de66cfac1e78de233e5f5b42e635
caijiMK <1395354790@qq.com>
2023-02-21 09:43:58 +0800

    修复一些 Bug，添加建立题目文件夹功能

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "eaves",
-    version="0.0.2",
-    authors = "Mine_King",
-    author_email = "1395354790@qq.com",
-    description = "一个基于 python 的轻量级比赛创建辅助工具。",
-    url = "https://github.com/sxyz-eaves/eaves",
-    packages = find_packages(),
-    package_data = {
-        'eaves': [
-            'SampleContest/*.*'
-        ]
-    }
+	name = "eaves",
+	version="0.0.2",
+	authors = "Mine_King",
+	author_email = "1395354790@qq.com",
+	description = "一个基于 python 的轻量级比赛创建辅助工具。",
+	url = "https://github.com/sxyz-eaves/eaves",
+	packages = find_packages(),
+	package_data = {
+		'eaves': [
+			'SampleContest/*.*', 'SampleContest/.*',
+			'SampleProblem/*.*', 'SampleProblem/.*',
+			'SampleProblem/data', 'SampleProblem/sample', 'SampleProblem/tables', 'SampleProblem/users'
+		]
+	}
 )
