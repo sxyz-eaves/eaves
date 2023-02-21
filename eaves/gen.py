c7e7d47ab2efb22866061eec77d66362af729abc
shiyihang2007 <467557146@qq.com>
2023-02-21 08:19:35 +0800

    添加注释

--- eaves/gen.py
+++ eaves/gen.py
@@ -21,9 +21,11 @@ def genContest():
     if len(sys.argv) == 2:
         base.missingParameter()
     try:
+        # 尝试创建文件
         shutil.copytree(os.path.split(__file__)[0] + '/' + 'SampleContest',
                         os.getcwd() + '/' + sys.argv[2])
     except FileExistsError:
+        # 捕获 文件已经存在 错误
         log.error(u"该文件夹已经存在")
         # 这里会把子文件复制进去吗?
 
@@ -34,10 +36,12 @@ def genProblem():
     for i in range(2, len(sys.argv)):
         dirPath = os.getcwd() + '\\' + sys.argv[i]
         try:
+            # 尝试创建文件
             log.info(u"正在创建文件夹 " + dirPath)
             os.makedirs(dirPath)
             log.warn(u"未完成!将不会创建子文件")
         except FileExistsError:
+            # 捕获 文件已经存在 错误
             log.error(u"该文件夹已经存在")
 
 
